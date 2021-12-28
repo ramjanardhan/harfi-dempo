d29b01c2569ad8c66027c4de4a187289fa3ae45e
unknown <support@RSAICLVAM271.ind.rsa-ins.com>
2021-12-28 17:28:15 +0530

    initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("Test");
+	}
+
+	@RequestMapping ("/")
+	public String message()
+	{
+		return "Hello World";
+	}
+	
+	@RequestMapping ("/message")
+	public String message1()
+	{
+		return "Hello World from AKS";
+	}
+	
+	
+}
